9c0e99617bfdf3fc5ba776b61c2d5ae37a1758c9
grindlemire <grindlemire@gmail.com>
2023-01-13 00:45:00 -0700

    update git cmd

--- src/git.ts
+++ src/git.ts
@@ -67,6 +67,7 @@ export async function pushVersionBranch(option: Option, config: Config, version:
         await checkoutBranch(branch, has == false);
         await exec.exec("git fetch -p");
         await mergeBranch(config.branch.baseBranch);
+        await exec.exec("git config pull.rebase false");
         await exec.exec(`git pull origin ${branch}`);
         await exec.exec(`git push ${remote} HEAD:${branch}`);
         await checkoutBranch(config.branch.baseBranch, false);
@@ -81,6 +82,7 @@ export async function pushVersionBranch(option: Option, config: Config, version:
         await checkoutBranch(branch, has == false);
         await exec.exec("git fetch -p");
         await mergeBranch(config.branch.baseBranch);
+        await exec.exec("git config pull.rebase false");
         await exec.exec(`git pull origin ${branch}`);
         await exec.exec(`git push ${remote} HEAD:${branch}`);
         await checkoutBranch(config.branch.baseBranch, false);
